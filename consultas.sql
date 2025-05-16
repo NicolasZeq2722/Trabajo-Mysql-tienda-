cbec57691d6475ce663260c8542c3b3aad1bb6f4
Nicolas zequea <nicomansolov89@gmail.com>
2025-05-15 19:18:12 -0500

    3

--- consultas.sql
+++ consultas.sql
@@ -1,3 +1,5 @@
+/*Seccion 1*/
+
 /*1:
 select nombre from producto*/
 
@@ -88,3 +90,92 @@ where precio > 200 and codigo_fabricante = 6;*/
 select nombre from producto
 WHERE codigo_fabricante = 1 OR codigo_fabricante = 3 OR codigo_fabricante = 5;*/
 
+/*28
+select nombre from producto
+where codigo_fabricante IN (1,3,5);*/
+
+/*29
+select nombre, precio*100 as centimos from producto*/
+
+/*30
+select nombre from fabricante where nombre like 's%';*/
+
+/*31
+select nombre from fabricante where nombre like 'e%';*/
+
+/*32
+select nombre from fabricante where nombre like '%w%';*/
+
+/*33
+select nombre from fabricante where nombre like '____';*/
+
+/*34
+select nombre from producto where nombre like '%Portátil';*/
+
+/*35
+select nombre from producto where nombre like '%Monitor' AND precio<215;*/
+
+/*36
+select nombre, precio from producto 
+where precio>=180 order by precio desc, nombre asc;*/
+
+
+/*Seccion 2*/
+
+/*1
+select p.Nombre AS 'NombreProducto', p.precio, f.nombre AS 'NombreFabricante'
+FROM producto p INNER JOIN fabricante f ON codigo_fabricante = f.codigo*/
+
+/*2
+select p.nombre AS 'NombreProducto', p.precio, f.nombre AS 'NombreFabricante'
+from producto p INNER JOIN fabricante f ON codigo_fabricante = f.codigo order by f.nombre ASC;*/
+
+/*3
+select p.codigo_fabricante AS 'CodigoFabricante', p.nombre AS 'ProductoNombre', f.codigo AS 'FabricanteProducto', f.nombre AS 'FabricanteNombre'
+from producto as p INNER JOIN fabricante as f ON p.codigo_fabricante = f.codigo*/
+
+/*4
+select p.nombre AS 'NombreProducto', p.precio AS 'PrecioProducto', f.nombre AS 'FabricanteNombre'
+from producto as p INNER JOIN fabricante as f ON p.codigo_fabricante = f.codigo order by p.precio*/
+
+/*5
+select p.nombre AS 'NombreProducto', p.precio AS 'PrecioProducto', f.nombre AS 'FabricanteNombre'
+from producto as p INNER JOIN fabricante as f ON p.codigo_fabricante = f.codigo order by p.precio DESC;*/
+
+/*6
+select p.nombre 
+from producto as p INNER JOIN fabricante as f ON p.codigo_fabricante = f.codigo where f.nombre = 'Lenovo'*/
+
+/*7
+select p.nombre from producto as p INNER JOIN fabricante as f ON p.codigo_fabricante = f.codigo where f.nombre = 'Crucial' AND p.precio > 200*/
+
+/*8
+select p.nombre from producto as p INNER JOIN fabricante as f ON p.codigo_fabricante = f.codigo 
+WHERE f.nombre = 'Asus' OR f.nombre = 'HewlettPackard' OR f.nombre = 'Seagate'*/
+
+/*9
+select p.nombre from producto as p INNER JOIN fabricante as f ON p.codigo_fabricante = f.codigo 
+WHERE f.nombre IN ('Asus', 'HewlettPackard', 'Seagate');*/
+
+/*10
+select p.nombre, p.precio 
+from producto as p INNER JOIN fabricante as f ON p.codigo_fabricante = f.codigo
+where f.nombre like '%e'*/
+
+/*11
+select p.nombre, p.precio 
+from producto as p INNER JOIN fabricante as f ON p.codigo_fabricante = f.codigo
+where f.nombre like '%w%';*/
+
+/*12
+select p.nombre , p.precio, f.nombre 
+from producto as p INNER JOIN fabricante as f ON p.codigo_fabricante = f.codigo 
+where p.precio >=180 ORDER BY p.precio DESC, p.nombre ASC*/
+
+/*13
+select f.codigo, f.nombre 
+from producto as p INNER JOIN fabricante as f ON p.codigo_fabricante = f.codigo 
+where f.codigo IN (select p.codigo_fabricante from producto)*/
+
+
+/*Seccion 3*/
